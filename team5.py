83e78fe526f4fb36aa98b31ce38bac22ef95eeca
Eliram1 <58487251+Eliram1@users.noreply.github.com>
2019-12-09 13:37:22 -0700

    Update team5.py

--- team5.py
+++ team5.py
@@ -7,29 +7,28 @@
 ####
 
 team_name = 'potential' # Only 10 chars displayed.
-strategy_name = 'stbrehkthch'
+strategy_name = ''
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+   
+    global betray
+    global collude
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    if my_history == "c":
-        return "b"
+     if their_history == 'c' and betray%4 == 0 and collude%4 == 0:
+      collude += 1
+      return 'b'
+    elif their_history == 'c' and betray%4 == 0 and not collude%4 == 0:
+      collude += 1
+      return 'c'
+    elif their_history == 'b': 
+      collude += 1
+      betray += 1
+      return 'b'
     else:
-        return "c"
+      collude += 1
+      betray += 1
+      return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
